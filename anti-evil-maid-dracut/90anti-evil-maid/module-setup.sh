de2b9f180c4c8362bd3b1e6fe14ef52da84238c7
Rusty Bird <rustybird@openmailbox.org>
2015-07-30 17:46:18 +0000

    Indent code, sort dependencies

--- anti-evil-maid-dracut/90anti-evil-maid/module-setup.sh
+++ anti-evil-maid-dracut/90anti-evil-maid/module-setup.sh
@@ -1,53 +1,48 @@
 #!/bin/bash
 
 check() {
-
-which tpm_unsealdata  >/dev/null 2>&1 || return 1
-
+    which tpm_unsealdata  >/dev/null 2>&1 || return 1
 }
+
 
 #depends() {
 #}
 
+
 installkernel() {
-
-instmods tpm_tis
-
+    instmods tpm_tis
 }
 
-
 install() {
+    inst_rules "$moddir/69-anti-evil-maid.rules"
+    inst_script "$moddir"/anti-evil-maid /sbin/anti-evil-maid
+    inst_script "$moddir"/anti-evil-maid-check-mount-devs /sbin/anti-evil-maid-check-mount-devs
 
-inst_rules "$moddir/69-anti-evil-maid.rules"
-inst_script "$moddir"/anti-evil-maid /sbin/anti-evil-maid
-inst_script "$moddir"/anti-evil-maid-check-mount-devs /sbin/anti-evil-maid-check-mount-devs
+    # TPM software stack
+    dracut_install \
+        clear \
+        cut \
+        file \
+        /usr/share/misc/magic \
+        grep \
+        sha1sum \
+        sort \
+        tcsd \
+        tpm_pcr_extend \
+        tpm_unsealdata \
+        wc
 
-# TPM software stack
-dracut_install \
-tpm_unsealdata \
-tpm_pcr_extend \
-sha1sum \
-cut \
-sort \
-wc \
-tcsd \
-file \
-clear \
-/usr/share/misc/magic \
-grep
+    dracut_install \
+        $systemdsystemunitdir/anti-evil-maid-console.service \
+        $systemdsystemunitdir/anti-evil-maid-plymouth.service \
+        $systemdsystemunitdir/anti-evil-maid-check-mount-devs.service \
+        $systemdsystemunitdir/initrd.target.wants/anti-evil-maid-console.service \
+        $systemdsystemunitdir/initrd.target.wants/anti-evil-maid-plymouth.service \
+        $systemdsystemunitdir/initrd.target.requires/anti-evil-maid-check-mount-devs.service
 
-dracut_install \
-$systemdsystemunitdir/anti-evil-maid-console.service \
-$systemdsystemunitdir/anti-evil-maid-plymouth.service \
-$systemdsystemunitdir/anti-evil-maid-check-mount-devs.service \
-$systemdsystemunitdir/initrd.target.requires/anti-evil-maid-check-mount-devs.service \
-$systemdsystemunitdir/initrd.target.wants/anti-evil-maid-console.service \
-$systemdsystemunitdir/initrd.target.wants/anti-evil-maid-plymouth.service
-
-# all this crap below is needed for tcsd to start properly...
-dracut_install ip
-inst_simple "$moddir"/hosts /etc/hosts
-inst_simple "$moddir"/passwd /etc/passwd
-inst_simple "$moddir"/shadow /etc/shadow
-
+    # all this crap below is needed for tcsd to start properly...
+    dracut_install ip
+    inst_simple "$moddir"/hosts /etc/hosts
+    inst_simple "$moddir"/passwd /etc/passwd
+    inst_simple "$moddir"/shadow /etc/shadow
 }
